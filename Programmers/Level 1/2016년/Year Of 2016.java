eab793519d7d024edb7e073a49cbe058a31eef90
Donghyeon0915 <63500239+Donghyeon0915@users.noreply.github.com>
2020-04-12 23:27:25 +0900

    Create Year Of 2016.java

---
+++ Programmers/Level 1/2016년/Year Of 2016.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+class Solution {
+  public String solution(int a, int b) {
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+
+        Calendar startDate = new GregorianCalendar(2016,0,1);
+        Calendar lastDate = new GregorianCalendar(2016,a - 1, b);
+        long distance = (lastDate.getTimeInMillis() - startDate.getTimeInMillis()) / (24 * 60 * 60 * 1000);
+        
+        String answer = days[(int)distance % 7];
+        
+        return answer;
+  }
+}
