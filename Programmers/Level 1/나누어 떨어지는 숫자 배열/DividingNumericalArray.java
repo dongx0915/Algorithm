9c45f4ea9b02d7d62f0cc726e5d761ea72981066
Donghyeon0915 <63500239+Donghyeon0915@users.noreply.github.com>
2020-04-13 20:07:26 +0900

    Update DividingNumericalArray.java

--- Programmers/Level 1/나누어 떨어지는 숫자 배열/DividingNumericalArray.java
+++ Programmers/Level 1/나누어 떨어지는 숫자 배열/DividingNumericalArray.java
@@ -1,5 +1,5 @@
 class Solution {
   public int[] solution(int[] arr, int divisor) {
-        return Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray(); // 람다식을 이용해 divisor로 나눠지는 배열의 원소를 
+        return Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray(); // 람다식을 이용해 divisor로 나눠지는 배열의 원소를 추출
   }
 }
