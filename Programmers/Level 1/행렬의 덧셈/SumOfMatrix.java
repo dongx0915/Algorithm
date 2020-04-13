38d4987e6e6515543ff82d93a1212389c06dfb56
Donghyeon0915 <63500239+Donghyeon0915@users.noreply.github.com>
2020-04-14 00:20:50 +0900

    Create SumOfMatrix.java

---
+++ Programmers/Level 1/행렬의 덧셈/SumOfMatrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int row = arr1.length, col = arr1[0].length;
+        int[][] answer = new int[row][col];
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
+        return answer;
+    }
+}
