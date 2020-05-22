acdf2b630cf208ad232e9742357ecb543d7b0f54
Donghyeon0915 <63500239+Donghyeon0915@users.noreply.github.com>
2020-05-22 23:49:52 +0900

    Update StartAndLink.java

--- BaekJoon Online Judge/Brute-Force(완전탐색)/스타트와 링크/StartAndLink.java
+++ BaekJoon Online Judge/Brute-Force(완전탐색)/스타트와 링크/StartAndLink.java
@@ -3,82 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BaekJoon;
+package BruteForce;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *
+ * 스타트와 링크 https://www.acmicpc.net/problem/14889
  * @author MSI
  */ 
+
 public class StartAndLink {
     public static int[][] abillity;
-    public static int[] player;
     public static boolean[] visit;
     public static int min = Integer.MAX_VALUE;
     
     public static int getSumDiffrent(ArrayList<Integer> picked, int pickCnt){
-        int sum1 = 0, sum2 = 0;
-        int[] TeamStart = new int[(pickCnt / 2) + 1];
-        int[] TeamLink = new int[(pickCnt / 2) + 1];
+        int sum1 = 0, sum2 = 0, index = 0;
+        int[] TeamStart = new int[pickCnt];
+        int[] TeamLink = new int[pickCnt];
         
-        for(int i = 0; i < pickCnt / 2; i++){
-            TeamStart[i] = picked.get(i);
-        }
-        TeamStart[pickCnt / 2] = picked.get(0);
+        for (int i = 0; i < pickCnt; i++) TeamStart[i] = picked.get(i);
+        for(int j = 0; j < pickCnt * 2; j++)if(!visit[j]) TeamLink[index++] = j;
         
-        for (int i = 0; i < pickCnt / 2; i++) {
-            TeamLink[i] = picked.get(i + (pickCnt / 2));
-        }
-        TeamLink[pickCnt / 2] = picked.get(pickCnt / 2);
-//        
-//        for (int i : TeamLink) {
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
-//        
-//        for (int i : TeamStart) {
-//            System.out.print(i + " ");
-//        }
-//        System.out.println("");
-//        System.out.println("");
-            
-        for (int i = 0; i < pickCnt / 2; i++) {
-            sum1 += (TeamStart[i] + TeamStart[i + 1]);
-        }
-    
         
-        for (int i = 0; i < pickCnt / 2; i++) {
-            sum2 += (TeamLink[i] + TeamLink[i + 1]);
+        for(int i = 0; i < pickCnt - 1; i++){
+            for(int j = i + 1; j < pickCnt; j++){
+                sum1 += abillity[TeamStart[i]][TeamStart[j]] + abillity[TeamStart[j]][TeamStart[i]];
+                sum2 += abillity[TeamLink[i]][TeamLink[j]] + abillity[TeamLink[j]][TeamLink[i]];
+            }
         }
-            
+
         return Math.abs(sum1 - sum2);
     }
-    
-    public static void print(ArrayList<Integer> picked){
-        picked.forEach((s)->{
-            System.out.print(s + " ");
-        });
-        System.out.println("");
-    }
-    
-    public static void combination(ArrayList<Integer> picked, int pickCnt){
+
+    public static void combination(ArrayList<Integer> picked, int n, int pickCnt){
         if(picked.size() == pickCnt){
-            int result = getSumDiffrent(picked, pickCnt);
-            min = Integer.min(min, result);
+            min = Integer.min(min, getSumDiffrent(picked, pickCnt));
             //print(picked);
             return;
         }
         
         int last = picked.isEmpty() ? -1 : picked.get(picked.size() - 1);
         
-        for (int i = 0; i < pickCnt; i++) {
-            if(!visit[i]){
+        for (int i = 0; i < n; i++) {
+            if(!visit[i] && last < i){
                 visit[i] = true;
                 picked.add(i);
                 
-                combination(picked, pickCnt);
+                combination(picked, n, pickCnt);
                 
                 visit[i] = false;
                 picked.remove(picked.get(picked.size()-1));
@@ -91,22 +64,17 @@ public class StartAndLink {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // 선수의 수
         abillity = new int[n][n];
-        player = new int[n];
         visit = new boolean[n];
         
         ArrayList<Integer> picked = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            player[i] = i;
-        }
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 abillity[i][j] = sc.nextInt();
             }
         }
         
-        combination(picked, n);
+        combination(picked, n, n / 2);
         System.out.println(min);
     }
     
