5a5c8b21815dbc4c36bd803a12fed8058eef92e9
donghyeon0915 <donghyeon0915@naver.com>
2020-12-11 23:59:31 +0900

    Add CraneClawMachineGame.java

---
+++ Programmers/Level 1/크레인 인형뽑기 게임/CraneClawMachineGame.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package craneclawmachinegame;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Donghyeon <20183188>
+ */
+public class CraneClawMachineGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int solution(int[][] board, int[] moves) {
+        Stack<Integer> picked = new Stack<>();
+        String[] character = {"네오", "달걀", "공룡", "어피치", "프로도"};
+        
+        int answer = 0;
+        int row = board[0].length;
+        int col = board.length;
+        
+        for (int move : moves) {
+            int lastPick = 0;
+            for (int i = 0; i < col; i++) {
+                if(board[i][move-1] != 0){
+                    if(!picked.isEmpty()) lastPick = picked.peek();
+                    if(lastPick == board[i][move-1]){
+                        System.out.println(character[lastPick - 1]);
+                        answer++;
+                        picked.pop();
+                    }
+                    else picked.push(board[i][move -1]);
+                    board[i][move-1] = 0;
+                    
+                    break;
+                }
+            }
+        }
+        
+        return answer * 2;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //같은 숫자는 같은 캐릭터를 나타냄
+        // (0,3) - 1
+        // (4,1) - 5
+        // (moves - 1, max)
+        int[][] board = {{0,0,0,0,0},
+                         {0,0,1,0,3},
+                         {0,2,5,0,1},
+                         {4,2,4,4,2},
+                         {3,5,1,3,1}
+        };
+        
+        int[] moves = {1,5,3,5,1,2,1,4};
+        
+        System.out.println(solution(board, moves));
+    }
+
+}
