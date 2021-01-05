70e3e18dad1871ef626c0ec59538da7c89ad98ad
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-01-05 22:25:29 +0900

    Update MazeNavigation(Solving).java

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#2178 미로 탐색/MazeNavigation(Solving).java
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#2178 미로 탐색/MazeNavigation(Solving).java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication4;
+package MazeNavigation;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -24,55 +24,53 @@ class Point {
     }
 }
 
-public class JavaApplication4 {
+public class MazeNavigation {
 
     /**
      * @param args the command line arguments
      */
     static int col, row;
     static String[] maze;
-    static int[] dx = {0, 1, 0, -1};
+    static int[] dx = {0, 1, 0, -1};                    //상, 우, 하, 좌
     static int[] dy = {-1, 0, 1, 0};
     static boolean[][] visited;
 
     public static boolean checkRoute(int x, int y) {
         if (x < 0 || x >= col || y < 0 || y >= row) {
-            return false; //범위를 벗어나면 false]=
+            return false;                               //범위를 벗어나면 false
         }
         if (visited[x][y]) {
-            return false;         //이미 방문한 곳이면 false
+            return false;                               //이미 방문한 곳이면 false
         }
         return maze[x].charAt(y) != '0';
     }
 
     public static int searchShortestRoute(String[] maze) {
-        System.out.println("들어옴");
-        int dist = 1;
+        int dist = 1;                                                                              //시작부터 1칸이므로 1로 초기화
         boolean isArrived = false;
         Queue<Point> q = new LinkedList<>();
 
-        q.add(new Point(0, 0));
+        q.add(new Point(0, 0));                                                                    //시작 지점 삽입
         visited[0][0] = true;
 
         while (!q.isEmpty()) {
-            int thisTurnSize = q.size();
-            System.out.println("이번 턴 사이즈 : " + thisTurnSize);
-            for (int i = 0; i < thisTurnSize; i++) {
+            int thisTurnSize = q.size();                                                           //thisTurnSize : 현재 턴에 갈 수 있는 경로의 수
+            dist++;
+            for (int i = 0; i < thisTurnSize; i++) {                                                
                 Point nowP = q.poll();
                 
                 for (int j = 0; j < 4; j++) {
                     Point nextP = new Point(nowP.x + dx[j], nowP.y + dy[j]);
-                    if(nextP.x == col && nextP.y == row) {isArrived = true; break;}
-                    if (checkRoute(nextP.x, nextP.y)) {
+                    if(nextP.x == col - 1 && nextP.y == row - 1) {isArrived = true; break;}         //도착 지점이면 break
+                    if (checkRoute(nextP.x, nextP.y)) {                                             //갈 수 있는 경로인지 체크
+                        visited[nextP.x][nextP.y] = true;
                         q.add(nextP);
                     }
                 }
                 if(isArrived) break;
             }
             if(isArrived) break;
-            dist++;
         }
-        
         return dist;
     }
 
