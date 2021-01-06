32136c743154380f81972a64e03feea0ceb1a596
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-01-06 22:51:10 +0900

    Update SnakeAndLadderGame(Memory Excced).java

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#16928 뱀과 사다리 게임/SnakeAndLadderGame(Memory Excced).java
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#16928 뱀과 사다리 게임/SnakeAndLadderGame(Memory Excced).java
@@ -17,8 +17,8 @@ public class SnakeAndLadderGame{
     public static final int ARRIVE = 100;
     
     public static int getArrivePos(int start){
-        for (Obstacle pos : obstacle) if(pos.start == start) return pos.end;
-        return start;
+        for (Obstacle pos : obstacle) if(pos.start == start) return pos.end;        //전달받은 인자(주사위를 굴려 이동할 위치)가 사다리나 뱀을 포함하고 있으면 도착지점 리턴
+        return start;                                                               //아무것도 없으면 그냥 리턴
     }
     
     public static int MinDiceCntToBFS(){
@@ -26,18 +26,18 @@ public class SnakeAndLadderGame{
         Queue<Integer> dice = new LinkedList<>();
         int diceCnt = 0;
         
-        posit.add(1);
-        dice.add(diceCnt);
+        posit.add(1);                                       //시작지점 위치 삽입
+        dice.add(diceCnt);                                  //주사위 횟수 삽입(0)
         
         while(!posit.isEmpty()){
             int nowPos = posit.poll();
             diceCnt = dice.poll();
             
-            if(getArrivePos(nowPos) >= ARRIVE) break;
+            if(getArrivePos(nowPos) >= ARRIVE) break;       //현재 위치가 도착지점이면 break
             
             for (int i = 1; i <= 6; i++) {
-                posit.add(getArrivePos(nowPos + i));
-                dice.add(diceCnt + 1);
+                posit.add(getArrivePos(nowPos + i));       //현재 위치에서 주사위를 굴렸을 때(1~6) 도착지점을 큐에 삽입
+                dice.add(diceCnt + 1);                     //주사위 횟수 +1
             }
         }
 
