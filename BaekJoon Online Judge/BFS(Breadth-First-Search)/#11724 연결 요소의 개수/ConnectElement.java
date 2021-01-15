3a63787cd3e7f898b614d4bf0c061f8b651f38e3
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-01-15 23:47:09 +0900

    Create ConnectElement.java

---
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#11724 연결 요소의 개수/ConnectElement.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ *
+ * @author W44858
+ */
+
+public class Element {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static int vortex;       // N
+    public static int edge;         // M
+    public static int[][] adjacent; // adjacent matrixt
+    public static boolean[] visited;
+    
+    public static int bfs(int root){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            visited[now] = true;
+            
+            for (int i = 1; i < vortex + 1; i++) {
+                if(adjacent[now][i] == 1 && !visited[i]){
+                    q.add(i);
+                }
+            }
+        }
+        
+        return 1;
+    }
+    
+    public static int getConnectionNode(){
+        int connected = 0;
+        
+        for (int i = 1; i < vortex + 1; i++) {
+            if(!visited[i]) {
+                connected += bfs(i);
+            }
+        }
+        
+        return connected;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        vortex = sc.nextInt();
+        edge = sc.nextInt();
+        adjacent = new int[vortex + 1][vortex + 1];
+        visited = new boolean[vortex + 1];
+        
+        for (int i = 0; i < edge; i++) {
+            int d1= sc.nextInt();
+            int d2= sc.nextInt();
+            adjacent[d1][d2] = adjacent[d2][d1] = 1;
+        }
+        for (int i = 1; i < vortex + 1; i++) {
+            adjacent[i][i] = 1;
+        }
+        System.out.println(getConnectionNode());
+    }
+    
+}
