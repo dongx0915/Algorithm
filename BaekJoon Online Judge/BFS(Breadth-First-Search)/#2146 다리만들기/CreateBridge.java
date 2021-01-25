4b83fb04e7e8ab921900eec451dc5feb75ae91af
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-01-25 23:56:47 +0900

    Update CreateBridge.java

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#2146 다리만들기/CreateBridge.java
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#2146 다리만들기/CreateBridge.java
@@ -39,14 +39,14 @@ public class CreateBridge {
             System.out.println("");
         }
     }
-    
-    public static void init_visited(){
-        for (boolean[] bs : visited) {
-            Arrays.fill(bs, false);
+        
+    public static void init_visited(){              //매 경우 바다에 대한 visited를 초기화 해주어야함
+        for (boolean[] bs : visited) {              
+            Arrays.fill(bs, false);                 
         }
     }
     
-    public static boolean isEdge(Point next){       // 4 방향 모두 0이 아니면 가장자리가 아님
+    public static boolean isEdge(Point next){       // 4 방향 모두 0이 아니면 가장자리가 아님(0이 하나라도 있으면 가장자리)
         for (int i = 0; i < 4; i++) {
             Point nxt = new Point(next.x + dx[i], next.y + dy[i]);
             if(nxt.x < 0 || nxt.x >= N || nxt.y < 0 || nxt.y >= N) continue;
@@ -73,7 +73,7 @@ public class CreateBridge {
             for (int i = 0; i < 4; i++) {
                 Point next = new Point(now.x + dx[i], now.y + dy[i]);
                 if(next.x < 0 || next.x >= N || next.y < 0 || next.y >= N) continue;
-                if(map[next.x][next.y] != 0 && map[next.x][next.y] != root_idx) return dis;
+                if(map[next.x][next.y] != 0 && map[next.x][next.y] != root_idx) return dis;     //0이 아니고 root_idx(현재 섬의 번호)랑 다르면 다른 섬에 도착한 것
                 if(map[next.x][next.y] != 0) continue;                      // 틀린이유 : 여기서 isEdge를 체크했음
                 if(!visited[next.x][next.y]){
                     q.add(next);
@@ -83,7 +83,7 @@ public class CreateBridge {
             }
         }
         
-        return 9999;
+        return 9999;                    //9999대신 다른 
     }
     
     public static int getShortestBridge(){
