9ee9e6a77df97b685bd53a8dc37a7e004110dd27
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-01-30 16:57:21 +0900

    Update LostBracket.java

--- BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#1541 잃어버린 괄호/LostBracket.java
+++ BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#1541 잃어버린 괄호/LostBracket.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package LostBracket;
+package Baekjoon.LostBracket;
 
 import java.util.Scanner;
 
@@ -23,24 +23,26 @@ public class Main {
         
         for (int i = 1; i < operate.length; i++) {
             minus_turn = operate[i].equals("-") ^ minus_turn;       //xor 연산
-                
+            
             if(minus_turn) temp += Integer.parseInt(num[i]);        // - > +, + > - 인 경우엔 그냥 temp에 더해두기
             else{                                                   // + > +, - > - 인 경우
                 if(temp != 0){                                      // 기존의 temp를 result에 부호를 반전시켜 더해주기    
                     temp += Integer.parseInt(num[i]);
-                    temp *= -1;
-                    result += temp;
+                    result -= temp;
                     temp = 0;
                 }
-                if(operate[i].equals("-"))minus_turn = true;        // - > -인 경우 minus_turn을 true로 고쳐주기
-                else result += Integer.parseInt(num[i]);            // + > +인 경우는 그냥 result에 더하기
+                else temp += Integer.parseInt(num[i]);
+                
+                if(operate[i].equals("-")) minus_turn = true;       // - > -인 경우 minus_turn을 true로 고쳐주기
+                else {                                              // + > +인 경우는 그냥 result에 더하기    
+                    result += Integer.parseInt(num[i]);             // 위에서 temp != 0이면 temp에 num[i]를 더해주는데
+                    temp = 0;                                       // temp는 -가 나온 경우에만 사용되는 변수이다. 따라서 +가 나왔으면 temp를 초기화해준다.
+                }            
             }
         }
         
-        if(temp != 0){                                              //반복문이 끝나고 temp가 남아있으면 result에서 빼주기
-            temp *= -1;
-            result += temp;
-        }
+        if(temp != 0) result -= temp;                                //반복문이 끝나고 temp가 남아있으면 result에서 빼주기
+            
         return result;
     }
     public static void main(String[] args) {
@@ -53,5 +55,4 @@ public class Main {
         System.out.println(getMinResult(num, oper));
         
     }
-    
 }
