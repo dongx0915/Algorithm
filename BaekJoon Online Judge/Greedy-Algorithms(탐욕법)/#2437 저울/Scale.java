7155d89df1c45cfae87f0b0d68e95d29284e7bf1
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-02-05 23:20:09 +0900

    Create Scale.java

---
+++ BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#2437 저울/Scale.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Greedy.Baekjoon2437;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Donghyeon <20183188>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int getMinWeight(int n, int[] weight){
+        int min_weight = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if(weight[i] > min_weight + 1) return min_weight + 1;
+            else min_weight += weight[i];
+        }
+        
+        return min_weight + 1;
+    }
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] weight = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            weight[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(weight);
+        System.out.println(getMinWeight(n, weight));
+
+    }
+    
+}
