549f82140e6ea1fb5f907c70171ce0591a3a50b4
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-02-12 23:38:01 +0900

    Update Sensor.java

--- BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#2212 센서/Sensor.java
+++ BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#2212 센서/Sensor.java
@@ -41,22 +41,21 @@ public class Main {
  
         Arrays.sort(section);
         
-        for (int i = 0; i < k - 1; i++) {index[i] = section[i].index;}
-        index[k - 1] = n - 1;
-        
+        for (int i = 0; i < k - 1; i++) {index[i] = section[i].index;}  //기준이 될 인덱스를 배열에 저장
+        index[k - 1] = n - 1;                                           //마지막 인덱스를 저장해줌        
+                                                                        //[0, 1, 5] 이면 (0,1) + (2,5)가 됨
         Arrays.sort(index);
         
         int sum = 0;
         int cnt = 0;
         for (int i = 0; i < n - 1; i++) {
-            if(i == index[cnt]){
-                cnt++;
+            if(i == index[cnt]){           // (0<= i < 1) + (2 <= i < 5) 처럼 더해져야 하므로 현재 i가 index[cnt](기준 인덱스)와 같아지면 
+                cnt++;                     // 거리를 더하지 않고 넘어간다. 추가로 인덱스도 하나 시켜준다.(다음 기준 인덱스로 설정)
                 continue;
             }
             sum += section_int[i];
         }
 
-        
         return sum;
     }
     
@@ -72,7 +71,7 @@ public class Main {
         }
         
         Arrays.sort(sensor);
-        if(n < k) System.out.println("0");
+        if(n < k) System.out.println("0");      //센서의 개수보다 집중국의 개수가 많은 경우는 각 센서마다 집중국을 설치하면 되므로 무조건 거리의 합은 0이 된다.
         else System.out.println(getReceiveableArea(n, k, sensor));
     }
     
