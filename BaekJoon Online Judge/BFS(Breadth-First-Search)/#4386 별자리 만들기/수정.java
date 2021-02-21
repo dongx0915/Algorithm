36045b1f1b50c173903f71ce0ff614dcd60cd204
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-02-21 22:46:59 +0900

    Update 수정.java

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#4386 별자리 만들기/수정.java
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#4386 별자리 만들기/수정.java
@@ -1,4 +1,4 @@
-/*
+	/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -24,26 +24,20 @@ class Point{
 }
 
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static double getDistance(Point s1, Point s2){
         return Math.sqrt(Math.pow(s1.x - s2.x, 2) + Math.pow(s1.y - s2.y , 2));
     }
     
     public static void setDistance(int n, double[][] dis, Point[] star){
-        
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                //if(i == j) continue;
                 if(dis[i][j] == -1) dis[i][j] = dis[j][i] = getDistance(star[i], star[j]);
             }
         }
     }
     
     public static double bfs(int root, int n, double[][] dis, Point[] star){
-        double cost = 0;
+        double cost = Double.MAX_VALUE;
         double temp = 0;
         Queue<Integer> q = new LinkedList<>();
         Queue<Integer> cnt_q = new LinkedList<>();
@@ -56,24 +50,29 @@ public class Main {
         while(!q.isEmpty()){
             int now = q.poll();
             int cnt = cnt_q.poll();
+            System.out.println("cnt = " + cnt);
             if(cnt == n) {
-                Double.min(temp, cost);
+                System.out.println("들어오나");
+                cost = Double.min(temp, cost);
                 temp = 0;
+                continue;
             }
             
-            
+            System.out.println("root = " + (root + 1));
             for (int i = 0; i < n; i++) {
-                if(dis[root][i] != -1 && !visited[i]){
-                    //System.out.printf("root = %d i = %d\n", root, i);
+                if(i == root) continue;
+                if(dis[root][i] != -1 && !visited[i]){          //visited가 있으면 안될듯하다.
                     q.add(i);
                     cnt_q.add(cnt + 1);
                     visited[i] = true;
-                    temp += getDistance(star[now], star[i]);
+                    temp += dis[now][i];
+                    //getDistance(star[now], star[i]);
                     //System.out.println("Cost = " + cost);
                 }
             }
+            System.out.println("");
         }
-        
+        cost = Double.min(temp, cost);
         return cost;
     }
     public static double getMinimumCost(int n, double[][] dis, Point[] star){
