e00665d53da3d555646b05710e231a4bca2357c4
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-05-27 23:42:27 +0900

    Create Friend(Wrong Answer).java

---
+++ BaekJoon Online Judge/Graph/다익스트라(Dijkstra)/#1058 친구/Friend(Wrong Answer).java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DFS.BOJ1058;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Donghyeon <20183188>
+ */
+class Node implements Comparable<Node>{
+    int index_;
+    int dis_;
+
+    public Node(int index_, int dis_) {
+        this.index_ = index_;
+        this.dis_ = dis_;
+    }
+    
+    @Override
+    public int compareTo(Node n){
+        return this.dis_ - n.dis_;
+    }
+}
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static final int INF = 100;
+    public static int[][] adj;
+    public static int[] dis;
+    
+    
+    public static int dijkstra(int root, int n){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        
+        dis[root] = 0;
+        pq.offer(new Node(root, dis[root]));
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            if(cur.dis_ > dis[cur.index_]) continue;
+            
+            for (int i = 0; i < n; i++) {
+                if(adj[cur.index_][i] != INF){
+                    if(dis[i] > dis[cur.index_] + adj[cur.index_][i]){
+                        dis[i] = dis[cur.index_] + adj[cur.index_][i];
+                        pq.offer(new Node(i, dis[i]));
+                    }
+                }
+            }
+            
+        }
+        
+        int friend_cnt = 0;
+        
+        for (int i = 0; i < n; i++) if(root != i && 1 <= dis[i] && dis[i] != INF) friend_cnt++;
+        return friend_cnt;
+    }
+    
+    public static int getTwoFriends(int n){
+        int twoFriends = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < n; i++) {
+            dis = new int[n];
+            Arrays.fill(dis, INF);
+            
+            twoFriends = Math.max(twoFriends, dijkstra(i, n));
+        }
+        
+        return twoFriends;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();       //사람 수
+        
+        
+        adj = new int[n][n];
+
+        for (int i = 0; i < n; i++) Arrays.fill(adj[i], INF);
+        
+        for (int i = 0; i < n; i++){
+            String input = sc.next();
+            for (int j = 0; j < input.length(); j++) {
+                adj[i][j] = input.charAt(j) == 'Y' ? 1 : INF;
+            }
+        }
+        
+        System.out.println(getTwoFriends(n));
+    }
+    
+}
