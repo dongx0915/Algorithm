2f4f6d1d2b853ab384045e4514d5af13e755a35d
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-06-09 23:03:37 +0900

    Update and rename Cheese(Wrong Answer).java to Cheese.java

--- BaekJoon Online Judge/DFS(Depth-First-Search)/#2638 치즈/Cheese.java
+++ BaekJoon Online Judge/DFS(Depth-First-Search)/#2638 치즈/Cheese.java
@@ -32,8 +32,8 @@ public class Main {
     public static int[] dx = {-1, 0, 1, 0};
     public static int[] dy = { 0,-1, 0, 1};
     public static int[][] map;
-    public static Queue<Pair> areaQ = new LinkedList<>();
     public static Queue<Pair> cheeseQ = new LinkedList<>();
+    public static Queue<Pair> meltedQ = new LinkedList<>();
     
     public static boolean isRange(int x, int y){
         return !(x < 0 || y < 0 || x >= N || y >= M);
@@ -47,44 +47,55 @@ public class Main {
         areaDfs(x, y - 1);
         areaDfs(x + 1, y);
         areaDfs(x, y + 1);
+        
     }
     
-    public static void initArea(){              //외부 공간이 영역을 표시하는 메소드
-        for (int i = 0; i < N; i++) 
-            for (int j = 0; j < M; j++) 
-                if(map[i][j] == 0) areaDfs(i,j);
-    }   
-    
     public static void initArea(int x, int y) {              //외부 공간이 영역을 표시하는 메소드
-        areaDfs(0,0);
+        areaDfs(x,y);
     }
     //치즈에 둘러싸인 영역을 체크하는 방법이 필요함
     
     public static void meltCheese(){                        //녹을 치즈를 표시하고, 치즈를 녹임
-        System.out.println(cheeseQ.size());
-        int index = 0;
+        //System.out.println(cheeseQ.size());
         int size = cheeseQ.size();
         
         for (int i = 0; i < size; i++) {                    //이번에 녹는 치즈를 표시
             Pair now = cheeseQ.poll();
+            int air_cnt = 1;
             
             for (int j = 0; j < 4; j++) {
                 Pair next = new Pair(now.x_ + dx[j], now.y_ + dy[j]);
-                if(map[next.x_][next.y_] == -1) map[now.x_][now.y_]++;
+                if(map[next.x_][next.y_] == -1) air_cnt++;
             }
+            
+            map[now.x_][now.y_] = air_cnt;
             cheeseQ.add(now);
         }
             
+//        printMap();
+//        System.out.println("");
+        
         for (int i = 0; i < size; i++) {                    //치즈를 녹임
             Pair cheese = cheeseQ.poll();
             
             if(map[cheese.x_][cheese.y_] >= 3){
-                map[cheese.x_][cheese.y_] = -1;
+                map[cheese.x_][cheese.y_] = 0;              //녹은 치즈를 Queue에 저장
+                meltedQ.add(cheese);
                 continue;
             }
             cheeseQ.add(cheese);
         }
         
+    }
+    
+    public static void printMap(){
+        for (int[] is : map) {
+            for (int i : is) {
+                if(i == -1) System.out.print(" #");
+                else System.out.printf("%2d", i);
+            }
+            System.out.println("");
+        }
     }
     
     public static void main(String[] args) {
@@ -102,23 +113,22 @@ public class Main {
             }
                 
         
-        initArea(0,0); //처음 외부 공간 영역을 -1로 셋팅함
-        int time = 1;
+        //initArea(0,0); //처음 외부 공간 영역을 -1로 셋팅함
+        int time = 0;
+        initArea(0, 0);
+        //printMap();
+        
         while(!cheeseQ.isEmpty()){
             meltCheese();
-            initArea();
+            //printMap();
+            while(!meltedQ.isEmpty()){
+                Pair cheese = meltedQ.poll();
+                initArea(cheese.x_, cheese.y_);
+            }
             time++;
         }
-        
-        
-        for (int[] is : map) {
-            for (int i : is) {
-                System.out.printf("%2d", i);
-            }
-            System.out.println("");
-        }
-        
-        System.out.println("time = " + time);
+
+        System.out.println(time);
     }
     
 }
