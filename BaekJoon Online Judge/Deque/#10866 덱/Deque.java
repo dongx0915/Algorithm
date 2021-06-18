462a687eabd7b672a5d97d57511d5e53905c9b36
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-06-18 23:20:07 +0900

    Update Deque.java

--- BaekJoon Online Judge/Deque/#10866 덱/Deque.java
+++ BaekJoon Online Judge/Deque/#10866 덱/Deque.java
@@ -5,70 +5,86 @@
  */
 package Deque;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Donghyeon <20183188>
  */
 class Deque{
-    int[] array;
-    int max_size;
-    int size;       //덱에 들어있는 데이터의 수
-    int front;      //front 포인터
-    int back;       //back 포인터
+    private final int[] array;
+    private final int max_size;
+    private int size;       //덱에 들어있는 데이터의 수
+    private int front;      //front 포인터
+    private int back;       //back 포인터
     
     public Deque(){
         this.array = new int[10000];
         this.max_size = this.array.length;
         this.size = 0;
-        this.front = 1;
+        this.front = 0;
         this.back = 0;
     }
     
     public void push_front_X(int data){
-        if(size >= 10000) return;
+        if(size >= max_size) return;
+        if(size == 0) back = (front + 1) % max_size;
+        array[front] = data;
         
         front--;
-        if(front < 0) front = this.array.length - 1;
-
-        array[front] = data;
+        if(front < 0) front = max_size - 1;
+        size++;
     }
     
     public void push_back_X(int data){
-        if(size >= 10000) return;
+        if(size >= max_size) return;
+        if(size == 0) front = (back - 1) < 0 ? max_size - 1 : (back - 1);
         
         array[back] = data;
         back = (back + 1) % max_size;
+        size++;
     }
     
     public int pop_front(){
         if(size <= 0) return -1;
-        int pop = array[0];
         
-        for (int i = 0; i < size - 1; i++) {
-            array[i] = array[i + 1];
-        }
-        
-        return pop;
+        front = (front + 1) % max_size;
+        size--;
+
+        return array[front];
     }
     
     public int pop_back(){
-        return -1;
+        if(size <= 0) return -1;
+        
+        back--;
+        if(back < 0) back = max_size - 1;
+        size--;
+        
+        return array[back];
     }
     
     public int size(){
         return this.size;
     }
     
-    public boolean empty(){
-        return this.size == 0;
+    public int empty(){
+        return this.size == 0 ? 1 : 0;
     }
     
     public int front(){
-        return this.array[this.front];
+        if(size == 0) return -1;
+        
+        return array[(front + 1) % max_size];
     }
     
     public int back(){
-        return this.array[this.size - 1];
+        if(size == 0) return -1;
+        
+        return (back - 1) < 0 ? array[max_size - 1] : array[back - 1];
     }
 }
 
@@ -76,9 +92,50 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        Deque dq = new Deque();
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch(cmd){
+                case "push_front" : 
+                    dq.push_front_X(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back" :
+                    dq.push_back_X(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front" :
+                    System.out.println(dq.pop_front());
+                    break;
+                case "pop_back" :
+                    System.out.println(dq.pop_back());
+                    break;
+                case "size"  :
+                    System.out.println(dq.size());
+                    break;
+                case "empty" :
+                    System.out.println(dq.empty());
+                    break;
+                case "front" :
+                    System.out.println(dq.front());
+                    break;
+                case "back" :
+                    System.out.println(dq.back());
+                    break;
+                default : 
+                    break;
+            }
+        }
+        
     }
     
 }
