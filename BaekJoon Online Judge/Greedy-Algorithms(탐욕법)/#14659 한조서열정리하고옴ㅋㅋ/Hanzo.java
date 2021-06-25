8167a19061c9b9930134c0747d51b84fe71b2efa
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-06-25 23:04:12 +0900

    Update Hanzo.java

--- BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#14659 한조서열정리하고옴ㅋㅋ/Hanzo.java
+++ BaekJoon Online Judge/Greedy-Algorithms(탐욕법)/#14659 한조서열정리하고옴ㅋㅋ/Hanzo.java
@@ -1,32 +1,22 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] peak = new int[n];
+        int max = 0, cnt = 0, prev = 0;
         
         for (int i = 0; i < n; i++) {
-            peak[i] = sc.nextInt();
-        }
-        
-        int max = 0;
-        int cnt = 0;
-        int prev = peak[0];
-        
-        for (int i = 1; i < n; i++) {
-            if(prev > peak[i]) cnt++;
+            int num = sc.nextInt();
+            if(prev > num) cnt++;
             else{
                 max = Math.max(cnt, max);
+                prev = num;
                 cnt = 0;
-                prev = peak[i];
             }
         }
         
-        max = Math.max(cnt, max);
-        System.out.println(max);
+        System.out.println(Math.max(cnt, max));
     }
-    
 }
