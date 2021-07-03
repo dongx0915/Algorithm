2bcc50ed4f7ee3f84f7b7b7315843f7132f7d29f
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-07-03 23:43:43 +0900

    Update 4Operations(Memory Exceed).java

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#14395 4연산/4Operations(Memory Exceed).java
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#14395 4연산/4Operations(Memory Exceed).java
@@ -5,9 +5,11 @@
  */
 package BFS.BOJ14395;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -15,11 +17,13 @@ import java.util.Scanner;
  */
 class Operator{
     Operator prev_;
-    int cur_;
-
-    public Operator(Operator prev, char cur) {
+    char cur_;
+    long s_;
+    
+    public Operator(Operator prev, char cur, long s) {
         this.prev_ = prev;
         this.cur_ = cur;
+        this.s_ = s;
     }
 }
 
@@ -28,65 +32,48 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static char[] oper = {'*', '+', '-', '/'};
-    
-    public static String bfs(int s, int t){
-        Queue<Integer> q = new LinkedList<>();
-        Queue<Operator> oper_q = new LinkedList<>();
+    public static String bfs(long s, int t){
+        Queue<Operator> q = new LinkedList<>();
         StringBuilder result = new StringBuilder();
+        Set<Long> visited = new HashSet<>();
+        char[] oper = {'*', '+', '-', '/'};
         
-        q.add(s);
-        oper_q.add(new Operator(null, ' '));
+        q.add(new Operator(null, ' ', s));
+        visited.add(s);
         
         while(!q.isEmpty()){
-            int now_s = q.poll();
-            Operator now_oper = oper_q.poll();
-            //System.out.println("now = " + now_s + " now_oper = " + (char)now_oper.cur_ + " prev = " + now_oper.prev_);
-            
-            if(now_s == t){
-               //System.out.println("now_oper = " + (char)now_oper.cur_ + " prev = " + now_oper.prev_);
-               while(now_oper.prev_ != null){
-                   result.append((char)now_oper.cur_);
-                   now_oper = now_oper.prev_;
+            Operator now = q.poll();
+
+            if(now.s_ == 0 || now.s_ < 0) continue;
+            if(now.s_ == t){
+               while(now.prev_ != null){
+                   result.append(now.cur_);
+                   now = now.prev_;
                }   
                return result.reverse().toString();
             }
             
+            long[] value = {now.s_ * now.s_, now.s_ + now.s_, 0, 1};
             
-            if(now_s == 0 || now_s > 1000000000 || now_s < 0) continue;
-            
-            if(now_s > 1) {
-                q.add(now_s * now_s);
-                oper_q.add(new Operator(now_oper, '*'));
+            for (int i = 0; i < 4; i++) {
+                if(value[i] <= t && !visited.contains(value[i])){
+                    visited.add(value[i]);
+                    q.add(new Operator(now, oper[i], value[i]));
+                }
             }
-            
-            
-            q.add(now_s + now_s);
-            oper_q.add(new Operator(now_oper, '+'));
-            
-            q.add(now_s - now_s);
-            oper_q.add(new Operator(now_oper, '-'));
-            
-            if(now_s != 0){
-                q.add(now_s / now_s);
-                oper_q.add(new Operator(now_oper, '/'));
-            }
-            
         }
         
         return "-1";
     }
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();
+        long s = sc.nextInt();
         int t = sc.nextInt(); //s > t로 바꿔야함
         //큰 수가 작은 수의 배수이면 변환 가능
         
         if(s == t) System.out.println("0");
         else System.out.println(bfs(s, t));
-
     }
-    
 }
