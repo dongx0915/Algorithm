bccbae9c2340340db854240d7eefea54b117d341
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-07-10 23:06:09 +0900

    Update PostfixExpression(Solving...).java

--- BaekJoon Online Judge/Stack/#1918 후위 표기식/PostfixExpression(Solving...).java
+++ BaekJoon Online Judge/Stack/#1918 후위 표기식/PostfixExpression(Solving...).java
@@ -21,6 +21,7 @@ public class Main {
         Stack<Character> s = new Stack<>();
         StringBuilder result = new StringBuilder();
         int length = exp.length();
+        int[] oper = {1,0,-1,0,-1,1}; //{*, +, _, -, _, /} (_는 빈 칸)
         
         /*
         * 42 : *
@@ -39,19 +40,16 @@ public class Main {
                     while(s.peek() != '(') result.append(s.pop());
                     s.pop();
                 }
-                else if(now == '*' || now == '/'){
-                    char top = s.peek();
-                        
-                    if(top == '*' || top == '/') result.append(s.pop());
-                    s.push(now);
-                }
-                else if(now == '+' || now == '-'){
-                    result.append(s.pop());
+                else{ //연산자인 경우
+                    //현재 연산자의 우선순위가 스택의 Top의 연산자보다 높거나 같을 때까지 Pop
+                    // -> 스택의 top에 현재 연산자보다 우선순위가 낮은 연산자가 나올 때까지 Pop
+                    while (!s.isEmpty() &&  s.peek() != '(' && oper[now - '*'] <= oper[s.peek() - '*']) result.append(s.pop());
                     s.push(now);
                 }
             }
-            System.out.println(result.toString());
         }
+        
+        while(!s.isEmpty()) result.append(s.pop());
         
         return result.toString();
     }
@@ -59,7 +57,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         String exp = new Scanner(System.in).next();
-        
+
         System.out.println(convertPreExpToPostExp(exp));
     }
     
