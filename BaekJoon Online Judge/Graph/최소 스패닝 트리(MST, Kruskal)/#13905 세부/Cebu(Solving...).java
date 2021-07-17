01d01f6aa2f92e45f2e20d7f905238589b2585d4
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-07-17 23:49:03 +0900

    Update Cebu(Solving...).java

--- BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#13905 세부/Cebu(Solving...).java
+++ BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#13905 세부/Cebu(Solving...).java
@@ -6,7 +6,9 @@
 package MST.BOJ13905;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -29,15 +31,20 @@ class Bridge implements Comparable<Bridge>{
     }
 }
 
-//class Edge{
-//    int index_;
-//    int weight_;
-//
-//    public Edge(int index_, int weight_) {
-//        this.index_ = index_;
-//        this.weight_ = weight_;
-//    }
-//}
+class Edge implements Comparable<Edge>{
+    int index_;
+    int w_;
+
+    public Edge(int index_, int weight_) {
+        this.index_ = index_;
+        this.w_ = weight_;
+    }
+    
+    @Override
+    public int compareTo(Edge e){
+        return e.w_ - this.w_;
+    }
+}
 
 public class Main {
 
@@ -48,8 +55,8 @@ public class Main {
     public static int m;       //다리의 수     - edge
     public static int s;       //숭이의 위치   - start
     public static int h;       //혜빈이의 위치 - end
-    //public static ArrayList<Edge>[] graph;
-    
+    public static ArrayList<Edge>[] graph;
+    public static final int INF = 1000001;
     
     public static int getParent(int x, int[] parent){
         if(parent[x] == x) return x;
@@ -71,53 +78,50 @@ public class Main {
         return x == y;
     }
     
-//    public static void setEdge(Bridge b){
-//        graph[b.home_[0]].add(new Edge(b.home_[1], b.weight_));
-//        graph[b.home_[1]].add(new Edge(b.home_[0], b.weight_));
-//    }
-//    
-//    public static int getPeperoCnt(int s, int h){
-//        Queue<Integer> q = new LinkedList<>();
-//        Queue<Integer> limitQ = new LinkedList<>();
-//        boolean[] visited = new boolean[graph.length];
-//        
-//        q.add(s);
-//        limitQ.add(Integer.MAX_VALUE);
-//        visited[s] = true;
-//        
-//        //최소 스패닝 트리(MST)에서는 사이클이 존재하지 않으므로
-//        //특정 노드(S) 특정 노드(H)까지 경로는 단 하나만 존재함
-//        
-//        while(!q.isEmpty()){
-//            int cur = q.poll();
-//            int limit = limitQ.poll();
-//            if(cur == h) return limit;
-//            
-//            for (Edge next : graph[cur]) {
-//                if(visited[next.index_]) continue; //이미 방문헀으면 continue
-//                if(limit >= next.weight_){
-//                    q.add(next.index_);
-//                    limitQ.add(next.weight_);
-//                    visited[next.index_] = true;
-//                }
-//            }
-//        }
-//        
-//        return -1;
-//    }
+    public static void setEdge(Bridge b){
+        graph[b.home_[0]].add(new Edge(b.home_[1], b.weight_));
+        graph[b.home_[1]].add(new Edge(b.home_[0], b.weight_));
+    }
     
+    public static int getPeperoCnt(int s, int h){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        int n = graph.length;
+        int[] w = new int[n];
+        boolean[] visited = new boolean[n];
+        Arrays.fill(w, INF);
+        
+        w[s] = INF;
+        visited[s] = true;
+        pq.offer(new Edge(s, w[s]));
+        
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            //if(w[cur.index_] != INF) continue; //이미 갱신이 된 경우
+            
+            for (Edge next : graph[cur.index_]) {
+                if(visited[next.index_]) continue;
+                //무게 제한이 작은 쪽의 값을 넣어줌
+                if(next.w_ >= w[cur.index_]) w[next.index_] = w[cur.index_];
+                else w[next.index_] = next.w_;
+                
+                visited[next.index_] = true;
+                pq.offer(new Edge(next.index_, w[next.index_]));
+                
+            }
+        }
+        
+        return w[h] == INF ? 0 : w[h];
+    }
+           
     public static int takeablePepero(ArrayList<Bridge> bridge, int[] parent, int s, int h){
         for (Bridge b : bridge) {
             if(isInGroup(b.home_[0], b.home_[1], parent)) continue;
             
-            if(b.home_[0] == h || b.home_[1] == h) return b.weight_;
-            //System.out.printf("[%d %d] 연결 w = %d\n", b.home_[0], b.home_[1], b.weight_);
             unionParent(b.home_[0], b.home_[1], parent);
-            
-            //setEdge(b);
+            setEdge(b);
         }
         
-        return -1;
+        return getPeperoCnt(s,h);
     }
     
     
@@ -132,12 +136,12 @@ public class Main {
         
         int[] parent = new int[n + 1];
         ArrayList<Bridge> bridge = new ArrayList<>();
-        //graph = new ArrayList[n + 1];
+        graph = new ArrayList[n + 1];
         
         
         for (int i = 0; i < n + 1; i++) {
             parent[i] = i;
-            //graph[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
         
         for (int i = 0; i < m; i++) bridge.add(new Bridge(sc.nextInt(), sc.nextInt(), sc.nextInt()));
