2d8e04bb759a4410e8cd26f2acfe1814d546fa6c
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-07-22 22:44:04 +0900

    Update SmallWorldNetwork(Solving...).java

--- BaekJoon Online Judge/Graph/플로이드 와샬(Floyd-Warshal)/#18243 Small World Network/SmallWorldNetwork(Solving...).java
+++ BaekJoon Online Judge/Graph/플로이드 와샬(Floyd-Warshal)/#18243 Small World Network/SmallWorldNetwork(Solving...).java
@@ -17,25 +17,27 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static final int INF = 100;
+    public static final int INF = 100000000;
     public static int[][] graph;
     
     public static String floyd(int n){
         for (int i = 0; i < n; i++) {
-            System.out.println("i == " + (i + 1));
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     if(j == i || k == i || j == k) continue;
-                    System.out.printf("[%d][%d] = %d  :: [%d][%d] + [%d][%d] = %d\n",
-                            j+1, k+1, graph[j][k], j+1, i+1, i+1, k+1, graph[j][i] + graph[i][k]);
-                    if(graph[j][k] > graph[j][i] + graph[i][k])
-                        graph[j][k] = graph[j][i] + graph[i][k];
+
+                    graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
                 }
             }
-            System.out.println("");
         }
         
-        for (int[] g : graph) for (int v : g) if(v != INF && v > 6) return "Big World!";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == j) continue;
+                if(graph[i][j] > 6) return "Big World!";
+            }
+        }
+        
         return "Small World!";
     }
     
@@ -54,20 +56,7 @@ public class Main {
             
             graph[n1][n2] = graph[n2][n1] = 1;
         }
-                for (int[] is : graph) {
-            for (int i : is) {
-                if(i == INF) i = 0;
-                System.out.print(i + " ");
-            }
-            System.out.println("");
-        }
+
         System.out.println(floyd(n));
-        for (int[] is : graph) {
-            for (int i : is) {
-                if(i == INF) i = 0;
-                System.out.print(i + " ");
-            }
-            System.out.println("");
-        }
     }
 }
