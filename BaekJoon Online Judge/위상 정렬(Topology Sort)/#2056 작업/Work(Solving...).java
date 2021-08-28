72a93284d6f64f4466cc2f9506cb20845caff77c
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-08-28 23:27:22 +0900

    Update Work(Solving...).java

--- BaekJoon Online Judge/위상 정렬(Topology Sort)/#2056 작업/Work(Solving...).java
+++ BaekJoon Online Judge/위상 정렬(Topology Sort)/#2056 작업/Work(Solving...).java
@@ -44,20 +44,21 @@ public class Main {
             if(q.isEmpty()) return -1;
             
             Work now = q.poll();
-            System.out.print((now.index_+1) + " ");
+            now.endTime_ += now.time_;
+            //System.out.print((now.index_+1) + " ");
+            max_time = Math.max(max_time, now.endTime_);
             
             //선행 작업이 없는 작업은 어떻게 처리할 지 ?
-            for (Integer next : now.preWork) {
-                if (--dimen[next] == 0) {
-                    //다음 작업의 종료 시간 = 이전 작업이 끝나는 시간 + 현재 작업에 소요되는 시간
-                    work[next].endTime_ = now.endTime_ + work[next].time_;
-                    q.add(work[next]);
-                    max_time = Math.max(max_time, work[next].endTime_);
-                }
-            }
+            now.preWork.stream().filter(next -> (--dimen[next] == 0)).map(next -> {
+                //다음 작업의 종료 시간 = 이전 작업이 끝나는 시간 + 현재 작업에 소요되는 시간
+                work[next].endTime_ = now.endTime_;
+                return next;
+            }).forEachOrdered(next -> {
+                q.add(work[next]);
+            });
         }
         
-        System.out.println("");
+        //System.out.println("");
         return max_time;
     }
     
@@ -76,7 +77,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             work[i].time_ = sc.nextInt();
             int preWork = sc.nextInt();
-            if(i == 0) work[i].endTime_ = work[i].time_; //첫번째 작업의 종료 시간을 설정(무조건 1번 작업이 처음이므로 시작 기준을 정해줘야함)
+            //if(preWork == 0) work[i].endTime_ = work[i].time_; //첫번째 작업의 종료 시간을 설정(무조건 1번 작업이 처음이므로 시작 기준을 정해줘야함)
             
             for (int j = 0; j < preWork; j++){
                 int w = sc.nextInt() - 1;       //w의 선행 작업이 i임
