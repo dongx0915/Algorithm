992e96d28f7e075743d01703f994ee64a62febdf
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-08-28 23:36:27 +0900

    Update Work(Solving...).java

--- BaekJoon Online Judge/위상 정렬(Topology Sort)/#2056 작업/Work(Solving...).java
+++ BaekJoon Online Judge/위상 정렬(Topology Sort)/#2056 작업/Work(Solving...).java
@@ -42,23 +42,18 @@ public class Main {
         
         for (int i = 0; i < N; i++) {
             if(q.isEmpty()) return -1;
-            
             Work now = q.poll();
+            
             now.endTime_ += now.time_;
-            //System.out.print((now.index_+1) + " ");
             max_time = Math.max(max_time, now.endTime_);
             
-            //선행 작업이 없는 작업은 어떻게 처리할 지 ?
             now.preWork.stream().filter(next -> (--dimen[next] == 0)).map(next -> {
                 //다음 작업의 종료 시간 = 이전 작업이 끝나는 시간 + 현재 작업에 소요되는 시간
                 work[next].endTime_ = now.endTime_;
                 return next;
-            }).forEachOrdered(next -> {
-                q.add(work[next]);
-            });
+            }).forEachOrdered(next -> { q.add(work[next]);});
         }
         
-        //System.out.println("");
         return max_time;
     }
     
@@ -69,15 +64,16 @@ public class Main {
         Work[] work = new Work[N];
         int[] dimen = new int[N];
         
-        /* 입력 형식
+        /* 
+        *  입력 형식
         *  시간 : 선행 작업 수 : 선행 작업 번호(선행 작업 수만큼)
         */
+        
         for (int i = 0; i < N; i++) work[i] = new Work(i);
         
         for (int i = 0; i < N; i++) {
             work[i].time_ = sc.nextInt();
             int preWork = sc.nextInt();
-            //if(preWork == 0) work[i].endTime_ = work[i].time_; //첫번째 작업의 종료 시간을 설정(무조건 1번 작업이 처음이므로 시작 기준을 정해줘야함)
             
             for (int j = 0; j < preWork; j++){
                 int w = sc.nextInt() - 1;       //w의 선행 작업이 i임
