c0e34d520cbc22f0605981a8be47fb16ebff4968
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-09-08 22:52:42 +0900

    Update StrahlerOrder.java

--- BaekJoon Online Judge/TopologySort(위상 정렬)/#9470 Strahler 순서/StrahlerOrder.java
+++ BaekJoon Online Judge/TopologySort(위상 정렬)/#9470 Strahler 순서/StrahlerOrder.java
@@ -5,10 +5,16 @@
  */
 package TopologySort.BOJ9470;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -66,25 +72,31 @@ public class Main {
         return river[M-1].order_;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int test = Integer.parseInt(br.readLine());
         Node[] river;
         int[] di;
         
         while(test-- != 0) {
-            K = sc.nextInt();
-            M = sc.nextInt();
-            P = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            K = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            P = Integer.parseInt(st.nextToken());
             di = new int[M];
             river = new Node[M];
             
             for (int i = 0; i < M; i++) river[i] = new Node();
             
             for (int i = 0; i < P; i++) {
-                int r1 = sc.nextInt()-1;
-                int r2 = sc.nextInt()-1;
+                st = new StringTokenizer(br.readLine());
+                int r1 = Integer.parseInt(st.nextToken())-1;
+                int r2 = Integer.parseInt(st.nextToken())-1;
                 
                 //그래프에 Node 클래스를 직접 추가하는 방식으로하면 간선의 개수만큼 인스턴스가 생성됨
                 //또한, 같은 번호의 노드라도 여러개의 인스턴스가 생성되어 각각 다른 값을 가질 수도 있음
@@ -92,8 +104,10 @@ public class Main {
                 di[r2]++; 
            }
             
-           System.out.println(K + " " + topologySort(river, di));
+           bw.write(K + " " + topologySort(river, di));
+           bw.write("\n");
         }
+        bw.flush();
     }
     
 }
