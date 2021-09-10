146254b3dea12ac99560f075a91ff894a8bb68c2
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-09-10 23:49:43 +0900

    Update PoketmonMaster.java

--- BaekJoon Online Judge/Set-Map By Hashing(해시를 사용한 집합과 맵)/#1620 나는야 포켓몬 마스터 이다솜/PoketmonMaster.java
+++ BaekJoon Online Judge/Set-Map By Hashing(해시를 사용한 집합과 맵)/#1620 나는야 포켓몬 마스터 이다솜/PoketmonMaster.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Hash.BOJ1620;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.StringTokenizer;
-
 
 public class Main {
 
@@ -22,25 +15,26 @@ public class Main {
         // TODO code application logic here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder result = new StringBuilder();
         
         int N = Integer.parseInt(st.nextToken());   //포켓몬의 개수
         int M = Integer.parseInt(st.nextToken());   //문제의 개수
-        HashMap<Integer, String> mapI = new HashMap<>();
-        HashMap<String, Integer> mapS = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        String[] poket = new String[N + 1];
         
         String name;
         for (int i = 1; i <= N; i++) {
             name = br.readLine();
-            mapI.put(i, name);
-            mapS.put(name, i);
+            map.put(name, i);
+            poket[i] = name;
         }
         
         String ques;
         for (int i = 0; i < M; i++) {
             ques = br.readLine();
-            if(ques.matches("^\\d+$")) System.out.println(mapI.get(Integer.parseInt(ques)));
-            else System.out.println(mapS.get(ques));
+            if(ques.matches("^\\d+$")) result.append(poket[Integer.parseInt(ques)]).append("\n");
+            else result.append(map.get(ques)).append("\n");
         }
+        System.out.print(result);
     }
-    
 }
