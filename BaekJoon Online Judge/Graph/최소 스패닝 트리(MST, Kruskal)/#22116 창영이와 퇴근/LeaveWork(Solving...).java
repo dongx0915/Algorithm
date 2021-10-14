8b5c76964bdf0e6355f85c8843a76e78c27206d6
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-10-14 23:52:03 +0900

    Update LeaveWork(Solving...).java

--- BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#22116 창영이와 퇴근/LeaveWork(Solving...).java
+++ BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#22116 창영이와 퇴근/LeaveWork(Solving...).java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Kruskal.BOJ22116;
+package Kruskcal.BOJ22116;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -12,14 +14,19 @@ import java.util.Scanner;
  *
  * @author Donghyeon <20183188>
  */
-class Edge{
-    int[] node_;
+class Edge implements Comparable<Edge>{
+    int[] node_ = new int[2];
     int slope_; //경사
 
     public Edge(int n1, int n2, int slope) {
         node_[0] = n1;
         node_[1] = n2;
         this.slope_ = slope;
+    }
+    
+    @Override
+    public int compareTo(Edge e){
+        return this.slope_ - e.slope_;
     }
 }
 
@@ -32,7 +39,7 @@ public class Main {
     public static int[][] map;
     public static PriorityQueue<Edge> edge;
     
-    public static void unionFind(int x, int y, int[] parent){
+    public static void unionParent(int x, int y, int[] parent){
         x = getParent(x, parent);
         y = getParent(y, parent);
         
@@ -52,8 +59,9 @@ public class Main {
         return x == y;
     }
     
-    public static PriorityQueue<Edge> setEdge(int N){
-        PriorityQueue<Edge> edge = new PriorityQueue<>();
+    public static ArrayList<Edge> setEdge(int N){
+        ArrayList<Edge> edge = new ArrayList<>();
+        boolean[] visit = new boolean[(N*N) + 1];
         int[] dy = { 0,-1, 0, 1};
         int[] dx = {-1, 0, 1, 0};
         
@@ -66,9 +74,13 @@ public class Main {
                 for (int k = 0; k < 4; k++) {
                     int ny = i + dy[k];
                     int nx = j + dx[k];
+                    int nextIndex = (N * ny) + nx;
                     
                     if(ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
-                    edge.add(new Edge(index, (N * ny) + nx, Math.abs(map[i][j] - map[ny][nx])));
+                    if(!visit[index] || !visit[nextIndex]){ //둘 중 하나라도 방문을 안했다면
+                        edge.add(new Edge(index, nextIndex, Math.abs(map[i][j] - map[ny][nx])));
+                        visit[index] = visit[nextIndex] = true;
+                    }
                 }
             }
         }
@@ -76,8 +88,21 @@ public class Main {
         return edge;
     }
     
-    public static void setMst(int N){
+    public static int getMaxSlope(int N, int[] parent){
+        ArrayList<Edge> edge = setEdge(N);
+        Collections.sort(edge);
         
+        int max = Integer.MIN_VALUE;
+        
+        for (Edge e : edge) {
+            if(isCycle(e.node_[0], e.node_[1], parent)) continue;
+            
+            
+            max = Math.max(max, e.slope_);
+            unionParent(e.node_[0], e.node_[1], parent);
+        }
+        
+        return max;
     }
     
     public static void main(String[] args) {
@@ -85,15 +110,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         
-        parent = new int[N + 1];
+        parent = new int[(N*N)+1];
         map = new int[N][N];
         
-        for (int i = 1; i < N+1; i++) parent[i] = i;
+        for (int i = 1; i < parent.length; i++) parent[i] = i;
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
+        
+        System.out.println(getMaxSlope(N, parent));
     }
 }
