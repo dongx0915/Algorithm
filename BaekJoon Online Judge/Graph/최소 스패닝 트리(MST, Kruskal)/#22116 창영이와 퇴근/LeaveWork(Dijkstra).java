92ce903821a599a69adb3bf3c013b1891b126211
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-10-23 23:45:25 +0900

    Update LeaveWork(Dijkstra).java

--- BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#22116 창영이와 퇴근/LeaveWork(Dijkstra).java
+++ BaekJoon Online Judge/Graph/최소 스패닝 트리(MST, Kruskal)/#22116 창영이와 퇴근/LeaveWork(Dijkstra).java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  *
  * @author Donghyeon <20183188>
  */
-class Edge {
+class Edge implements Comparable<Edge>{
     int index_;
     int d_;
 
@@ -22,11 +22,19 @@ class Edge {
         this.index_ = n;
         this.d_ = dis;
     }
-//    
-//    @Override
-//    public int compareTo(Edge e){
-//        return this.dis_ - e.dis_;
-//    }
+    
+    @Override
+    public int compareTo(Edge e){return this.d_ - e.d_;}
+}
+
+class Dis{
+    int d_;
+    int prev_;
+
+    public Dis(int dis_, int prev_) {
+        this.d_ = dis_;
+        this.prev_ = prev_;
+    }
 }
 
 public class Main {
@@ -35,7 +43,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static final int INF = 1_000_000_001;
-    public static int[] dis;
+    public static int N;
+    public static Dis[] dis;
     public static int[][] map;
     
     public static ArrayList<Edge>[] setEdge(int N){
@@ -53,9 +62,9 @@ public class Main {
                 for (int k = 0; k < 4; k++) {
                     int ny = i + dy[k];
                     int nx = j + dx[k];
-                    int nextIndex = (N * ny) + nx;
-                    
                     if(ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+                    
+                    int nextIndex = (N * ny) + nx;
                     edge[index].add(new Edge(nextIndex, Math.abs(map[i][j] - map[ny][nx])));
                 }
             }
@@ -64,36 +73,48 @@ public class Main {
         return edge;
     }
     
-    public static void dijkstra(int start, ArrayList<Edge>[] edge){
+    public static int dijkstra(int start, ArrayList<Edge>[] edge){
         PriorityQueue<Edge> pq = new PriorityQueue<>();
-        dis[start] = 0;
-        pq.add(new Edge(start, dis[start]));
+        dis[start].d_ = dis[start].prev_ = 0;
+        pq.add(new Edge(start, dis[start].d_));
 
         while(!pq.isEmpty()){
             Edge cur = pq.poll();
             
-            if(dis[cur.index_] < cur.d_) continue;
+            if(dis[cur.index_].d_ < cur.d_) continue;
             
             for (Edge e : edge[cur.index_]) {
-                
+                if(dis[e.index_].d_ > dis[cur.index_].d_ + e.d_){
+                    dis[e.index_].d_ = dis[cur.index_].d_ + e.d_;
+                    dis[e.index_].prev_ = cur.index_;
+                    
+                    pq.add(new Edge(e.index_, dis[e.index_].d_));
+                }
             }
         }
+        
+        Dis node = dis[N*N-1];
+        int maxHeight = 0;
+        
+        while(node.prev_ != 0){
+            maxHeight = Math.max(maxHeight, Math.abs(node.d_ - dis[node.prev_].d_));
+            node = dis[node.prev_];
+        }
+        
+        return maxHeight;
     }
         
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        N = sc.nextInt();
         
         map = new int[N][N];
-        dis = new int[N];
+        dis = new Dis[N*N];
         
-        Arrays.fill(dis, INF);
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                map[i][j] = sc.nextInt();
-            }
-        }
+        for (int i = 0; i < N*N; i++) dis[i]= new Dis(INF, 0);
+        for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) map[i][j] = sc.nextInt();
+
+        System.out.println(dijkstra(0, setEdge(N)));
     }
 }
