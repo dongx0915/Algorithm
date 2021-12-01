07009351bae49e5457c59a78acdfcff854c07b5d
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2021-12-01 19:04:43 +0900

    Update and rename Statistics(Solving...2).java to Statistics.java

--- BaekJoon Online Judge/Solved.ac (Class 문제)/Class 2/#2108 통계학/Statistics.java
+++ BaekJoon Online Judge/Solved.ac (Class 문제)/Class 2/#2108 통계학/Statistics.java
@@ -1,35 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package baekjoon;
+package Math.BOJ2108;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-/**
- *
- * @author Donghyeon <20183188>
- */
-public class Baekjoon {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); //N은 홀수
         int[] num = new int[N];
-        /*
-        * 산술평균 : N개의 수들의 합을 N으로 나눈 값
-        * 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값(N/2)
-        * 최빈값 : N개의 수들 중 가장 많이 나타나는 값(여러 개 있을 떄에는 최빈값 중 두 번째로 작은 값을 출력)
-        * 범위 : N개의 수들 중 최댓값과 최솟값의 차이
-        */
         int sum = 0;
         
         for (int i = 0; i < N; i++) sum += (num[i] = sc.nextInt());
@@ -43,29 +25,37 @@ public class Baekjoon {
             if(i < N-1) {
                 if (num[i] == num[i + 1]) freq++;
                 else {
+                    
                     if (max_freq < freq) {
                         max_freq = freq;
-                        freq = 1;
                         mode.clear();
                         mode.add(num[i]);
                     }
                     else if(max_freq == freq) mode.add(num[i]);
+                    freq=1;
                 }
             }
             else {
                 if(max_freq < freq){
                     max_freq = freq;
-                    freq = 1;
                     mode.clear();
                     mode.add(num[i]);
                 }
                 else if(max_freq == freq) mode.add(num[i]);
+                freq=1;
             }
         }
         
         Collections.sort(mode);
+        //System.out.println("최빈 값 : " + mode);
+        
+        
+        //System.out.println("");
         /* 산술평균 */
-        System.out.printf("%.0f\n", (double)sum / N);
+        double avg = (double)sum / N;
+        //avg = (avg > 0) ? avg + 0.5 : avg - 0.5;
+        
+        System.out.printf("%.0f\n", avg);
         
         /* 중앙값 */
         System.out.println(num[N/2]);
@@ -76,5 +66,4 @@ public class Baekjoon {
         /* 범위 */
         System.out.println(num[N-1] - num[0]);
     }
-    
 }
