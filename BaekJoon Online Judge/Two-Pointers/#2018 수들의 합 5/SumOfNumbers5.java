7bf4b492a3b66a4cc137db8608c11775899498c1
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2022-01-03 10:41:30 +0900

    Create SumOfNumbers5.java

---
+++ BaekJoon Online Judge/Two-Pointers/#2018 수들의 합 5/SumOfNumbers5.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TwoPointer.BOJ2018;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Donghyeon Kim <donghyeon0915@gmail.com> [20183188]
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static int solution(int N, int[] num){
+        int s = 0, e = 0;
+        int cnt = 0, sum = 0;
+        
+        while(s < N && e < N){
+            sum += num[e];
+            //System.out.println("s = " + s + " e = " + e + " sum = " + sum);
+            if(sum == N) cnt++;
+            
+            if(sum <= N) e++;
+            else {
+                sum -= num[s++];
+                sum -= num[e];
+            }
+            
+        }
+        
+        return cnt;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] num = new int[N];
+        
+        for (int i = 0; i < N; i++) num[i] = i+1;
+        
+        System.out.println(solution(N, num));
+    }
+    
+}
+
