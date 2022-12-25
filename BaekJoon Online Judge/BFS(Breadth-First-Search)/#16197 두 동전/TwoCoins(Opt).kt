94133b1249df91daf02df5e610795098c3b6b5f6
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2022-12-25 20:10:32 +0900

    Update TwoCoins(Opt).kt

--- BaekJoon Online Judge/BFS(Breadth-First-Search)/#16197 두 동전/TwoCoins(Opt).kt
+++ BaekJoon Online Judge/BFS(Breadth-First-Search)/#16197 두 동전/TwoCoins(Opt).kt
@@ -1,5 +1,3 @@
-package BOJ16197
-
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
@@ -14,22 +12,10 @@ val dy = arrayOf( 0,-1, 0, 1)
 fun Queue<Coin>.addCoin(next:Coin, cur:Coin) = this.add(if(map[next.x][next.y] == '#') cur else next)
 fun isDropped(n:Int, m:Int, p:Coin) = p.run { (x < 0 || y < 0 || x >= n || y >= m) }
 
-/**
- * o : 동전
- * . : 빈 칸
- * # : 벽
- * 동전이 이동하려는 칸이 벽(#)이면, 동전은 이동하지 않는다.
- * 동전이 이동하려는 방향에 칸이 없으면 동전은 보드 바깥으로 떨어진다.
- * 그 외의 경우에는 이동하려는 방향으로 한 칸 이동한다.이동하려는 칸에 동전이 있는 경우에도 한 칸 이동한다.
- */
 fun bfs(n:Int, m:Int, coin:ArrayList<Coin>):Int{
-    val coin1q = LinkedList<Coin>()
-    val coin2q = LinkedList<Coin>()
-    val cntQ = LinkedList<Int>()
-
-    coin1q.add(coin[0])
-    coin2q.add(coin[1])
-    cntQ.add(1)
+    val coin1q = LinkedList<Coin>().apply { add(coin[0]) }
+    val coin2q = LinkedList<Coin>().apply { add(coin[1]) }
+    val cntQ = LinkedList<Int>().apply { add(1) }
 
     while(!coin1q.isEmpty() && !coin2q.isEmpty()){
         val coin1 = coin1q.poll()
@@ -44,18 +30,16 @@ fun bfs(n:Int, m:Int, coin:ArrayList<Coin>):Int{
 
             val coin1Dropped = isDropped(n,m, next_coin1) // 범위 밖이면 떨어진 것
             val coin2Dropped = isDropped(n,m, next_coin2)
+            // 코인 하나만 떨어진 경우
+            if(coin1Dropped xor coin2Dropped) return cnt
 
-            if(coin1Dropped xor coin2Dropped){ // 코인 하나만 떨어진 경우
-                return cnt
-            } else { // 둘 다 떨어졌거나, 둘 다 안떨어진 경우
-                // 둘 다 안 떨어진 경우
-                if(!coin1Dropped && !coin2Dropped){
-                    coin1q.addCoin(next_coin1, coin1)
-                    coin2q.addCoin(next_coin2, coin2)
-                    cntQ.add(cnt + 1)
-                }
-                // 둘 다 떨어진 경우는 더 이상 진행 불가
+            // 둘 다 안 떨어진 경우
+            if (!coin1Dropped && !coin2Dropped) {
+                coin1q.addCoin(next_coin1, coin1)
+                coin2q.addCoin(next_coin2, coin2)
+                cntQ.add(cnt + 1)
             }
+            // 둘 다 떨어진 경우는 더 이상 진행 불가
         }
     }
 
