f39882ab99feb253e48cc7a5f50cc777602b9d69
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-01-01 20:33:25 +0900

    Create GiveHomework.kt

---
+++ BaekJoon Online Judge/Realization(구현)/#5597 과제 안 내신 분..?/GiveHomework.kt
@@ -0,0 +1,10 @@
+package BOJ5597
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+    val list = arrayListOf<Int>().apply { for (i in 1 .. 30) add(i) }
+
+    repeat(28){ list.remove(nextInt()) }
+    list.forEach { println(it) }
+}
