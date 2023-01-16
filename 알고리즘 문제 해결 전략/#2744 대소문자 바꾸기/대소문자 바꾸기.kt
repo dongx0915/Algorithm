2c92664233151bc833237cb740a276a31a37f050
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-01-17 02:47:50 +0900

    [Bronze V 2744] Title: 대소문자 바꾸기, Time: 112 ms, Memory: 14168 KB -NKLCBHub

---
+++ 알고리즘 문제 해결 전략/#2744 대소문자 바꾸기/대소문자 바꾸기.kt
@@ -0,0 +1,6 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println(readLine().map { ch -> if(ch in 'A' .. 'Z') ch + 32 else ch - 32 }.toCharArray())
+}
