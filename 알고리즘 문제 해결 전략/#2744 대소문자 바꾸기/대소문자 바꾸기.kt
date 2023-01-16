22d55201ec0b525476403637ce3d227d13a86a76
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-01-17 02:48:56 +0900

    Delete 알고리즘 문제 해결 전략/#2744 대소문자 바꾸기 directory

--- 알고리즘 문제 해결 전략/#2744 대소문자 바꾸기/대소문자 바꾸기.kt
+++
@@ -1,6 +0,0 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    println(readLine().map { ch -> if(ch in 'A' .. 'Z') ch + 32 else ch - 32 }.toCharArray())
-}
