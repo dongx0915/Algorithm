ee24a5669eec6743441bc9beae3c9f1e75a23e4a
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-01-17 02:49:11 +0900

    Delete 알고리즘 문제 해결 전략/#5597 과제 안 내신 분..? directory

--- 알고리즘 문제 해결 전략/#5597 과제 안 내신 분..?/과제 안 내신 분．．？.kt
+++
@@ -1,8 +0,0 @@
-import java.util.*
-
-fun main() = with(Scanner(System.`in`)){
-    val list = arrayListOf<Int>().apply { for (i in 1 .. 30) add(i) }
-
-    repeat(28){ list.remove(nextInt()) }
-    list.forEach { println(it) }
-}
