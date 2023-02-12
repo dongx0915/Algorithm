c04987b40047cff2df333126a78e5388cfdcb454
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-02-12 19:08:13 +0900

    [Silver II 11053] Title: 가장 긴 증가하는 부분 수열, Time: 236 ms, Memory: 26820 KB -NKLCBHub

---
+++ BaekJoon Online Judge/DP(Dynamic-Programming)/#11053 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
@@ -0,0 +1,23 @@
+import kotlin.math.max
+
+lateinit var dp:Array<Int>
+
+fun solve(list:List<Int>):Int{
+    for (i in list.indices){
+        // i = 먼저 선택되는 부분
+        for (j in i+1 until list.size){
+            if(list[j] > list[i])
+                dp[j] = max(dp[i] + 1, dp[j])
+        }
+    }
+
+    return dp.maxOrNull()!!
+}
+
+fun main(){
+    val n = readln().toInt()
+    val list = readln().split(" ").map { it.toInt() }
+    dp = Array(n){1}
+
+    println(solve(list))
+}
