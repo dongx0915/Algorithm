202619463f1f546fac3cb4c136ec14995e4ae321
Donghyeon-Kim <63500239+Donghyeon0915@users.noreply.github.com>
2023-03-28 04:12:21 +0900

    [Bronze V 27889] Title: 특별한 학교 이름, Time: 96 ms, Memory: 13188 KB -NKLCBHub

---
+++ BaekJoon Online Judge/Realization(구현)/#27889 특별한 학교 이름/특별한 학교 이름.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println(hashMapOf(
+        "NLCS" to "North London Collegiate School",
+        "BHA" to "Branksome Hall Asia",
+        "KIS" to "Korea International School",
+        "SJA" to "St. Johnsbury Academy"
+    )[readln()])
+}
