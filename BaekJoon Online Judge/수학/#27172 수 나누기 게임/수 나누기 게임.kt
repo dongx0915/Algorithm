96591fb6022cef072b0f459fc949c8bee2240426
Donghyeon Kim <63500239+Donghyeon0915@users.noreply.github.com>
2024-08-04 15:49:47 +0900

    [Gold V 27172] Title: 수 나누기 게임, Time: 1148 ms, Memory: 170460 KB -NKLCBHub

--- BaekJoon Online Judge/수학/#27172 수 나누기 게임/수 나누기 게임.kt
+++ BaekJoon Online Judge/수학/#27172 수 나누기 게임/수 나누기 게임.kt
@@ -1,5 +1,6 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.math.sqrt
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val peopleCnt = this.readLine().toInt()
@@ -16,11 +17,9 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
      */
     repeat(peopleCnt) {
         val myCard = cards[it]
-        for (j in 1 .. myCard) {
-            if (j * j > myCard) break
-            if (myCard % j == 0) { // 내 카드의 약수를 리스트로 관리
-                cardMeasures[it].add(j)
-            }
+        for (j in 1 .. sqrt(myCard.toDouble()).toInt()) {
+            // 내 카드의 약수를 리스트로 관리
+            if (myCard % j == 0) cardMeasures[it].add(j)
             if (j * j != myCard && myCard % j == 0) cardMeasures[it].add(myCard / j)
         }
     }
@@ -36,5 +35,5 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         }
     }
 
-    println(point.values.toString().replace(",", "").removeSurrounding("[", "]"))
+    println(point.values.joinToString(" "))
 }
